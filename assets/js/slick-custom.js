671a3ae971789764043267129f7a236b8ebdbbb5
stellaexp <kimshillam@icloud.com>
2020-12-28 21:09:21 +0000

    create sperate js file for custom slick carousel

---
+++ assets/js/slick-custom.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    $('.slick').slick({
+            dots: true,
+            infinite: true,
+            speed: 500,
+            fade: true,
+            cssEase: 'linear',
+            arrows: true
+        });
+});
