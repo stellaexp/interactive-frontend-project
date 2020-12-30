4dd0914b523a252ed375e1c69eeacea0b1f7bc19
stellaexp <kimshillam@icloud.com>
2020-12-30 22:35:58 +0000

    update js to make carousel responsive

--- assets/js/slick-custom.js
+++ assets/js/slick-custom.js
@@ -6,6 +6,33 @@ $(document).ready(function(){
             speed: 500,
             fade: true,
             cssEase: 'linear',
-            arrows: true
-        });
-});
+            arrows: true,
+            responsive: [
+                {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+                },
+                {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    arrows: false
+                }
+                },
+                {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false
+                }
+                }
+            ]
+    });
+})
