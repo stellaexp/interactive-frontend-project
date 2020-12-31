f08b952f7cb69f2ff0c82dcd5238fcabdb9e5e1f
stellaexp <kimshillam@icloud.com>
2020-12-31 09:11:46 +0000

    implement emailjs integration on contact page

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,5 +1,13 @@
 function sendMail(contactForm) {
-    emailjs.sendForm('service_olp5z8i', 'template_4v6wcfc', '#form')
+    emailjs.sendForm('service_olp5z8i', 'template_4v6wcfc', '#form', {
+        'first': contactForm.first.Value,
+        'last': contactForm.last.Value,
+        'email': contactForm.email.Value,
+        'option': contactForm.option.Value,
+        'group': contactForm.group.Value,
+        'datepicker': contactForm.datepicker.Value,
+        'notes': contactForm.notes.Value
+    })
     .then(
         function(response) {
             console.log("SUCCESS", response);
