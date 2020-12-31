aaf3e71cffa3314af1636917ceb5b79e5e9e8b44
stellaexp <kimshillam@icloud.com>
2020-12-31 15:56:09 +0000

    fix email js form not clearing on submit bug

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,33 +1,22 @@
-function ClearFields() {
-    document.getElementById("first").value = "";
-    document.getElementById("last").value = "";
-    document.getElementById("email").value = "";
-    document.getElementById("option").value = "";
-    document.getElementById("group").value = "";
-    document.getElementById("datepicker").value = "";
-    document.getElementById("total").value = "";
-    document.getElementById("notes").value = "";
-}
-
 function sendMail(contactForm) {
     emailjs.sendForm('service_olp5z8i', 'template_4v6wcfc', '#form', 'user_NjZWh4qnG3A9vjWb6Aa5u', {
-        'first': contactForm.first.Value,
-        'last': contactForm.last.Value,
-        'email': contactForm.email.Value,
-        'option': contactForm.option.Value,
-        'group': contactForm.group.Value,
-        'datepicker': contactForm.datepicker.Value,
-        'total': contactForm.notes.Value,
-        'notes': contactForm.notes.Value
-    })
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-            
-        },
-        function(error) {
-            console.log("FAILED", error);
-        }
-    );
-    return false;  // To block from loading a new page
-}  
+            'first': contactForm.first.Value,
+            'last': contactForm.last.Value,
+            'email': contactForm.email.Value,
+            'option': contactForm.option.Value,
+            'group': contactForm.group.Value,
+            'datepicker': contactForm.datepicker.Value,
+            'total': contactForm.total.Value,
+            'notes': contactForm.notes.Value
+        })
+        .then(
+            function(response) {
+                console.log("SUCCESS", response);
+                contactForm.reset();
+            },
+            function(error) {
+                console.log("FAILED", error);
+            }
+        );
+    return false; // To block from loading a new page
+}
